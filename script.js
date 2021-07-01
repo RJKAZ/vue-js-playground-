5679790b229f238f2180ab7a1b11b0f57e8aecf5
rjkaz <rjkaz@comcast.net>
2021-07-01 00:02:06 -0400

    starting instance lifecycle hooks

--- script.js
+++ script.js
@@ -76,3 +76,112 @@ var app6 = new Vue({
     message: 'Hello Vue!',
   },
 });
+
+// Another important concept in Vue is the component system which makes it easier to build larger scale applications with lots of smaller re-useable components.
+
+// In Vue, a component is essentially a Vue instance with pre-defined options.
+
+// An example, defining a new component called todo-item that accepts a prop
+
+Vue.component('todo-item', {
+  // The todo-item component now accepts a 'prop', which is like a custome attribute. This prop is called todo
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables'},
+      { id: 1, text: 'Cheese'},
+      { id: 2, text: 'Whatever else humans are supposed to eat'},
+    ]
+  }
+})
+
+
+
+// Now onto creating a Vue Instance - every Vue application starts by creating a Vue instance with the Vue function 
+
+var vm = new Vue({
+  //options
+})
+
+// Vue's design pattern was inspired by the MVVM pattern
+// MVVM = Model-view-viewmodel
+// following that convention, we often use the variable vm (short for View Model) to refer to our vue instance
+
+// When you create a Vue instance, you pass in an options object. The majority of this guide describes how you can use these options to create your desired behavior. 
+// A Vue application consists of a root Vue instance created with new Vue, optionally organzied into a tree of nested reusable components. 
+
+// Data and Methods
+
+// When a Vue instance is created, it adds all the properties found in its data object to Vue's reactivity system. 
+// When the values of those properties change, the view will "react" updating to match the new values
+
+// our data object
+var data = {a: 1}
+
+// Now lets add that data object to a vue instance
+
+var vm1 = new Vue({
+  data: data
+}) 
+
+// Getting the property on the instance returns the one from the original data
+vm1.a == data.a   // => true
+
+// Setting the property on the instance also affects the original data
+vm1.a = 2
+data.a            // => 2
+
+// and vice versa 
+data.a =3
+vm1.a           // => 3
+
+// when this data changes, the view will re-render. 
+// To note, properties in data are only reactive if they existed when the instance was created
+// this means you add a new property, likle
+
+vm1.b = 'hi'
+
+// Then changes to b will not trigger any view updates. If you know you'll need a property later, but it starts out empty or non-existant, you'll need to set some initial value
+// for example
+
+/*
+data: {
+  newTodoText: '',
+  visitCount: 0,
+  hideCompletedTodos: false,
+  todos: [],
+  error: null
+}
+*/
+
+/*
+var obj = {
+  foo: 'bar'
+}
+
+Object.freeze(obj)
+
+new Vue({
+  el: '#app-8',
+  data: obj
+})
+*/
+
+var data = { a: 1 }
+var vm = new Vue({
+  el: '#example',
+  data: data
+})
+
+vm.$data === data // => true
+vm.$el === document.getElementById('example') // => true
+
+// $watch is an instance method
+vm.$watch( 'a', function (newValue, oldValue) {
+  // this callback will be called when `vm.a` changes
+})
