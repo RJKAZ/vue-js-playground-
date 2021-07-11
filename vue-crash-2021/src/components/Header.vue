5bb59bd1babb6cd2ffb3e3fe20eb90dbedf7ba09
reese <rjkaz@comcast.net>
2021-07-11 17:04:28 -0400

    finished tutorial, but there are erros, compare to souce code and correct

--- vue-crash-2021/src/components/Header.vue
+++ vue-crash-2021/src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button 
+      v-show="HomePage" 
       @btn-click="$emit
       ('toggle-add-task')" 
       :text="showAddTask ? 'Close' : 'Add Task'"
@@ -21,8 +22,18 @@ export default {
     showAddTask: Boolean 
   },
   components: {
-    Button
-} 
+    Button,
+  },
+  computed: {
+    homePage() {
+      if(this.$route.path === '/') {
+        return true
+
+      } else {
+        return false 
+      }
+    }
+  }
 }
 </script>
 
